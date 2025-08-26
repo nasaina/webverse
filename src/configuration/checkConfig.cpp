42e84194cf68ad0114cbca707a93ba8fb3a1c031
Nasainarivelo Andrianarijaona <nandrian@c1r7s14.42antananarivo.mg>
2025-08-26 15:55:44 +0300

    error file

--- src/configuration/checkConfig.cpp
+++ src/configuration/checkConfig.cpp
@@ -6,7 +6,7 @@
 /*   By: nandrian <nandrian@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/26 10:21:02 by nandrian          #+#    #+#             */
-/*   Updated: 2025/08/26 13:04:18 by nandrian         ###   ########.fr       */
+/*   Updated: 2025/08/26 15:55:18 by nandrian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,11 +37,12 @@ void	checkConf(std::string filename)
 	{
 		if (it->find('{', 0) != std::string::npos)
 		{
-			// std::cout << Word(*it, 1) << std::endl;
 			if (Word(*it, 0) != "server" && Word(*it, 0) != "cgi"
 				&& Word(*it, 0) != "location")
 				throw std::runtime_error("Error: configuration file");
 			if ((Word(*it, 0) == "server" || Word(*it, 0) == "cgi") && (Word(*it, 1) != "{"))
+				throw std::runtime_error("Error: configuration file");
+			if ((Word(*it, 0) == "location") && (Word(*it, 2) != "{"))
 				throw std::runtime_error("Error: configuration file");
 		}
 		if (it->find('}', 0) != std::string::npos && Word(*it, 0) != "}")
@@ -49,5 +50,7 @@ void	checkConf(std::string filename)
 			std::cout << Word(*it, 0).c_str() << std::endl;
 			throw std::runtime_error("Error: configuration file");
 		}
+		if ((Word(*it, 0) == "location") && (Word(*it, 2) != "{"))
+			throw std::runtime_error("Error: configuration file");
 	}
 }
